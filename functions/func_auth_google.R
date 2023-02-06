71900de3ccbd5005400965275b8e5ff97da2ae4c
Alexandra Harris <103076913+alexandramharris@users.noreply.github.com>
2023-02-06 00:12:11 -0500

    Update func_auth_google.R

--- functions/func_auth_google.R
+++ functions/func_auth_google.R
@@ -2,3 +2,4 @@ auth_google <- function(email, service, token_path) {
   googlesheets4::gs4_auth(email = "alexandra.harris@timesunion.com", path = tokencodr::decrypt_token(service = service,
                                                           path = token_path,
                                                           complete = TRUE))
+  }
