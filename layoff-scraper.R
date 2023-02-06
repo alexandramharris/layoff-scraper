226e4a4b2b7742ba0062127c1ca533e05b171010
Alexandra Harris <103076913+alexandramharris@users.noreply.github.com>
2023-02-05 23:43:17 -0500

    Update layoff-scraper.R

--- layoff-scraper.R
+++ layoff-scraper.R
@@ -308,7 +308,7 @@ rename("Business type" = business_type,
 # Authorize for actions ----
 auth_google <- function(email, service, token_path) {
   googlesheets4::gs4_auth(email = "alexandra.harris@timesunion.com", path = tokencodr::decrypt_token(service = gsheet_layoffs,
-                                                                                                      path = .secret/gsheet_layoffs,
+                                                                                                      token_path = .secret/gsheet_layoffs,
                                                                                                       complete = TRUE))
 }
                    
